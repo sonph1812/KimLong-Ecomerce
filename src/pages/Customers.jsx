7d42f50db755e48c2dfafd02007e56c0245b7e15
anhtuan <nguyenanhtuan23122000@gmail.com>
2022-10-03 21:11:32 +0700

    list user

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -6,8 +6,9 @@ import { Header } from '../components';
 import { useSelector } from "react-redux";
 
 const Customers = () => {
-  const user = useSelector(state => state.userReducer.users)
-
+  const users = useSelector(state => state.userReducer.users)
+  const role = localStorage.getItem('role')
+  console.log(users);
   // const selectionsettings = { persistSelection: true };
   // const toolbarOptions = ['Delete'];
   // const editing = { allowDeleting: true, allowEditing: true };
@@ -38,29 +39,35 @@ const Customers = () => {
       <table className="min-w-full leading-normal ">
         <thead>
           <tr>
-            <td className="px-5 py-3 border-b-2  border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">Name</td>
-            <td className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">Email</td>
+            <td className="px-5 py-2 border-b-2  border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">Name</td>
+            <td className="px-2 py-2 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">Email</td>
+            <td className="px-2 py-2 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">Phone</td>
             <td className="px-2 py-2 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">Dob</td>
             <td className="px-2 py-2 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">Gender</td>
             <td className="px-2 py-2 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">Address</td>
-            <td colSpan={2} className=" text-center px-8 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider colSpan={'2'}"  >Action</td>
+            <td colSpan={2} className=" text-center px-4 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider colSpan={'2'}"  >Action</td>
           </tr>
         </thead>
-        {/* <tbody>
-        {isProductYet && products.map((product) => (
-          <tr key={product._id}>
-            <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">{product.name}</td>
-            <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">{product.price} $</td>
-            <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">{product.description}</td>
-            <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">{product.rating} </td>
-
-            <td className="px-2 py-2 border-b border-gray-200 bg-white text-sm">  <button className="absolute px-3 bg-green-400 opacity-50 rounded-full row hover:bg-green-800 focus:outline-none rounded">update</button> </td>
-            <td className="px-2 py-2 border-b border-gray-200 bg-white text-sm">  <button className="absolute px-3  bg-red-400 opacity-50 rounded-full row hover:bg-red-800 focus:outline-none rounded">delete</button> </td>
-
-          </tr>
-        ))}
-
-      </tbody> */}
+        <tbody>
+          {role &&
+            users.map((user, index) => (
+              <tr key = {user._id}>
+                <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">{user.name}</td>
+                <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">{user.email}</td>
+                <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">{user.phone} </td>
+                <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">{user.dob} </td>
+                <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">{user.gender} </td>
+                <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">{user.address} </td>
+                <td className="px-2 py-2 border-b border-gray-200 bg-white text-sm" >  <button style={{ position: "relative", left: "100px" }} class="bg-yellow-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                  Update
+                </button> </td>
+                <td className="px-2 py-2 border-b border-gray-200 bg-white text-sm">  <button style={{ position: "relative", left: "10px" }} class="bg-yellow-600 hover:bg-red-800 text-white font-bold py-2 px-4 rounded-full">
+                  Delete
+                </button> </td>
+              </tr>
+            ))
+          }
+        </tbody>
       </table>
     </div>
   );
