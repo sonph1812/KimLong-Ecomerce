18534be05e5f6ea13b29d95d6ce743a56f465cd1
anhtuan <nguyenanhtuan23122000@gmail.com>
2022-10-03 12:00:49 +0700

    ok

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -44,13 +44,6 @@ const Products = () => {
                     <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">{product.description}</td>
                     <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">{product.rating} </td>
 
-              {/* <td className=" text-center px-5 py-3 border-b border-gray-200 bg-white text-sm">
-                <button className="absolute px-10  bg-green-400 opacity-50 rounded-full row hover:bg-green-800 focus:outline-none rounded"
-                  onClick="">Edit</button>
-              </td>
-              <td className=" text-center px-5 py-3 border-b border-gray-200 bg-white text-sm">
-                <button className="absolute  px-10 bg-red-400 opacity-50 rounded-full row hover:bg-red-700 focus:outline-none rounded">Delete</button>
-              </td> */}
               <td className="px-2 py-2 border-b border-gray-200 bg-white text-sm">  <button className="absolute px-3 bg-green-400 opacity-50 rounded-full row hover:bg-green-800 focus:outline-none rounded">update</button> </td>
               <td className="px-2 py-2 border-b border-gray-200 bg-white text-sm">  <button className="absolute px-3  bg-red-400 opacity-50 rounded-full row hover:bg-red-800 focus:outline-none rounded">delete</button> </td>
 
