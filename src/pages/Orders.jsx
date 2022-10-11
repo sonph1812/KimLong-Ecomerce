e80c260395611f8ac1030a851ff726d8d80f60d7
sonph <sonph>
2022-10-11 13:52:07 +0700

    son add

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -5,6 +5,7 @@ import { Header } from '../components';
 
 const Orders = () => {
     const list = useSelector(s => s.orderReducer.orders)
+    console.log('tuan',list)
     return (
         <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
             <Header category="Page" title="Orders" />
