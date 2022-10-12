2e481a0e1c18de6bb5850bdc19fb7734fdb659e9
sonph <sonph>
2022-10-12 23:23:08 +0700

    son add bf merge tuan

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -12,16 +12,20 @@ const  UserProfile = ({userInfo}) => {
   const { currentColor } = useStateContext();
   const dispatch = useDispatch()
   const navigate = useNavigate()
+
   return (
     <div className="nav-item absolute right-1 top-16 bg-white dark:bg-[#42464D] p-8 rounded-lg w-96">
       <div className="flex justify-between items-center">
         <p className="font-semibold text-lg dark:text-gray-200">User Profile</p>
         <Button
+
           icon={<MdOutlineCancel />}
           color="rgb(153, 171, 180)"
           bgHoverColor="light-gray"
           size="2xl"
           borderRadius="50%"
+
+
 
         />
       </div>
