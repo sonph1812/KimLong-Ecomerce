d815894e9b0b44c90df7b51220941224b96104d2
anhtuan <nguyenanhtuan23122000@gmail.com>
2022-10-12 18:43:26 +0700

    643 fix cart

--- src/pages/user/CartPage.js
+++ src/pages/user/CartPage.js
@@ -9,6 +9,20 @@ import PageHero from '../../components/PageHero'
 
 const CartPage = () => {
   const items = useSelector(s => s.cartReducer.items)
+
+  if (items.length < 1) {
+    return (
+      <div className=" h-screen ">
+        <section className="section-center  text-center py-20">
+          <h2 className="text-4xl font-semibold mb-7">Your cart is empty</h2>
+          <Link to="/products">
+            <Btn name="Fill it up" />
+          </Link>
+        </section>
+      </div>
+    );
+  }
+
   return (
     <div>
       <PageHero title="Cart" />
